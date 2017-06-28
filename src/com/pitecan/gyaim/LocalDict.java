b791ca2bb79bbbbdb372d3343274634d77e05228
Toshiyuki Masui <masui@pitecan.com>
2017-06-28 09:56:21 +0900

    テストメソッド名に漢字を導入

--- src/com/pitecan/gyaim/LocalDict.java
+++ src/com/pitecan/gyaim/LocalDict.java
@@ -14,21 +14,25 @@ import java.io.IOException;
 
 import java.util.ArrayList;
 
-class DictEntry {
-    String pat, word;
-    int inConnection, outConnection;
-    int keyLink;
-    int connectionLink;
-
-    public DictEntry(String p, String w, int i, int o){
-	pat = p;
-	word = w;
-	inConnection = i;
-	outConnection = o;
-    }
-}
-
 public class LocalDict {
+    public class DictEntry {
+	String pat, word;
+	int inConnection, outConnection;
+	int keyLink;
+	int connectionLink;
+	
+	public DictEntry(String p, String w, int i, int o){
+	    pat = p;
+	    word = w;
+	    inConnection = i;
+	    outConnection = o;
+	}
+
+	public String word(){
+	    return word;
+	}
+    }
+
     static ArrayList<DictEntry> dict = new ArrayList<DictEntry>();
 
     public LocalDict(InputStream is){
@@ -46,15 +50,19 @@ public class LocalDict {
 		if(c == '#' || c == ' ' || c == '\t') continue; // コメント行
 		String[] a = line.split("\t",4);
 		if(a[3] == null || a[3] == "" || a[3].length() == 0 || ("" + a[3] == "")) a[3] = "0";
-		//System.out.println(a[0]);
+		//System.out.println(a[1]);
 		dict.add(new DictEntry(a[0],a[1],Integer.valueOf(a[2]),Integer.valueOf(a[3])));
             }
             br.close();
 	    in.close();
-	    System.out.println(dict.size());
+	    //System.out.println(dict.size());
 	    
 	} catch (IOException e) {  
 	    e.printStackTrace();  
 	}
+    }
+
+    public ArrayList<DictEntry> dict(){
+	return dict;
     }
 }
--- test/LocalDictTest.java
+++ test/LocalDictTest.java
@@ -1,12 +1,20 @@
 //
 // ローカル辞書検索のテスト
 //
+// JUnit 4.12を利用
+// compile:
+//    javac -classpath 'jar/junit-4.12.jar:jar/hamcrest-all-1.3.jar' ../src/com/pitecan/gyaim/LocalDict.java LocalDictTest.java
+// test:
+//    java -classpath "jar/junit-4.12.jar:jar/hamcrest-all-1.3.jar:.:../src" org.junit.runner.JUnitCore LocalDictTest
+//
+// @Test というアノテーションをつけるとテスト関数と解釈されるらしい
+//
+
 import static org.junit.Assert.*;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-
 
 import com.pitecan.gyaim.LocalDict;
 
@@ -16,20 +24,33 @@ import java.io.InputStream;
 import java.io.IOException;
 
 public class LocalDictTest {
+    static final String DICTFILE = "../assets/dict.txt";
+    static LocalDict localDict;
 
-    @Test
-    public void open(){
-	LocalDict dict;
-	File file = new File("../assets/dict.txt");
-	System.out.println(file);
+    public LocalDictTest(){
+	File file = new File(DICTFILE);
 	try {
 	    FileInputStream is = new FileInputStream(file);
-	    System.out.println("is = " + is);
-	    dict = new LocalDict(is);
+	    localDict = new LocalDict(is);
 	} catch (IOException e) {
-	    System.out.println("FileInputStream trouble");
+	    System.out.println("辞書読出し失敗");
 	    e.printStackTrace();  
 	}
-	assertEquals(10, 10);
+    }
+
+    @Test
+    public void 辞書のサイズが充分大きい(){
+	assertTrue(localDict.dict().size() > 10000);
+    }
+
+    @Test
+    public void 単語登録チェック(){
+	boolean 漢字が登録されてる = false;
+	boolean 増井が登録されてる = false;
+	for(LocalDict.DictEntry entry: localDict.dict()) {
+	    if(entry.word().equals("漢字")) 漢字が登録されてる = true;
+	    if(entry.word().equals("増井")) 増井が登録されてる = true;
+	}
+	assertTrue(漢字が登録されてる && 増井が登録されてる);
     }
 }
