de28a37b6df170eacff1b73fbb8159075c8bce54
Toshiyuki Masui <masui@pitecan.com>
2017-07-13 20:47:58 +0900

    Googleテスト用に抜けてたファイルを追加

---
+++ test/GoogleIMETest.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import com.pitecan.gyaim.GoogleIME;
+import com.pitecan.gyaim.Message;
+
+public class GoogleIMETest {
+    String 変換例リスト[][] = {
+	{"べんきょう", "勉強"},
+	{"ますい", "増井"},
+	{"いちのもと", "櫟本"},
+        {"むさしぼうべんけい","武蔵坊弁慶"},
+        {"かまくらのかいがん", "鎌倉の海岸"}
+    };
+
+    @Test
+    public void Google変換チェック() {
+        for (String[] 変換例: 変換例リスト) {
+	    boolean found = false;
+            for(String s: GoogleIME.convert(変換例[0])){
+		if(s.equals("")) break;
+		if(s.equals(変換例[1])) found = true;
+	    }
+            assertTrue(found);
+        }
+    }
+}
+
